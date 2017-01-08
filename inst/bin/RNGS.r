5f2fcc4866375cf6f04797e3430f07fe42970d50
aiminy <jlfmssm@gmail.com>
2017-01-08 17:43:29 -0500

    add STAR to perform alignment

--- inst/bin/RNGS.r
+++ inst/bin/RNGS.r
@@ -48,5 +48,120 @@
     cat("Finished downloading SRA ...\n")
 
   }else{
-        quit()
+
+    cat("Do you want to convert SRA files to fastq files?\n")
+
+    input<-file('stdin', 'r')
+    row <- readLines(input, n=1)
+
+    print(row)
+
+    if(row=="Yes") {
+
+      cat("please give input SRA file:\n")
+
+      input<-file('stdin', 'r')
+      input.file.dir <- readLines(input, n=1)
+
+      #input.file.dir=row
+
+      cat("please define the output file directory for this conversion:\n")
+
+      input<-file('stdin', 'r')
+      output.file.dir <- readLines(input, n=1)
+
+      R_lib=.libPaths()[1]
+
+      cmd1="bsub -P bbc -J \"fastq-dump\" -o %J.fastq-dump.log -e %J.fastq-dump.err -W 72:00 -n 8 -q general -u aimin.yan@med.miami.edu"
+      cmd2=paste0("fastq-dump --split-3 ",input.file.dir," ","-O ",output.file.dir)
+
+      system(paste0(cmd1," ",cmd2))
+
+      #cmd2=paste0()
+      #system(cmd2)
+
+      system("wait")
+      cat("Finished converting SRA ...\n")
+
+    }else{
+
+      cat("Do you want to use STAR to perform alignment?\n")
+
+      input<-file('stdin', 'r')
+      row <- readLines(input, n=1)
+
+      print(row)
+
+      if(row=="Yes") {
+
+
+        cat("Is your RNA-Seq data un-stranded?\n")
+
+        input<-file('stdin', 'r')
+        row <- readLines(input, n=1)
+
+        if(row=="Yes"){
+          strand="--outSAMstrandField introMotif"
+        }else
+        {
+         strand=""
+        }
+
+        cat("please give input fastq file dir:\n")
+
+        input<-file('stdin', 'r')
+        input.file.dir <- readLines(input, n=1)
+
+        library(RNGS)
+        re<-GetFastqFiles(input.file.dir)
+
+        #print(class(re))
+        #print(dim(re))
+        #print(re)
+
+         cat("please specify gene annotation file(GTF):\n")
+
+         input<-file('stdin', 'r')
+         input.gtf.file <- readLines(input, n=1)
+
+         cat("please specify STARindex file :\n")
+
+         input<-file('stdin', 'r')
+         STAR.index.file <- readLines(input, n=1)
+
+
+         cat("please define the output file directory for this alignment:\n")
+
+         input<-file('stdin', 'r')
+         output.file.dir <- readLines(input, n=1)
+        #
+        # R_lib=.libPaths()[1]
+
+         cat("please define the number of core to be used:\n")
+
+         input<-file('stdin', 'r')
+         Ncores <- readLines(input, n=1)
+
+
+          cmd1="bsub -P bbc -J \"STAR-alignment\" -o %J.STAR-alignment.log -e %J.STAR-alignment.err -W"
+          cmd2="72:00 -n 8 -q bigmem -R 'rusage[mem=36864] span[hosts=1]' -u aimin.yan@med.miami.edu"
+
+          cmd3=paste("STAR",strand,"--genomeLoad NoSharedMemory --runThreadN",Ncores,"--sjdbGTFfile",collapse = " ")
+          cmd4=paste(input.gtf.file,"--outFileNamePrefix",paste0(output.file.dir,"/STAR_"),"--genomeDir",STAR.index.file,collapse = " ")
+
+          for(i in 1:dim(re)[1]){
+           cmd5=paste("--readFilesIn",re[i,1],re[i,2],collapse = " ")
+           system(paste(cmd1,cmd2,cmd3,cmd4,cmd5,collapse = " "))
+          }
+
+        # #cmd2=paste0()
+        # #system(cmd2)
+        #
+        # system("wait")
+        # cat("Finished converting SRA ...\n")
+
+      }else{
+           quit()}
+    }
+
   }
