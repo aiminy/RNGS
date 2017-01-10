de80e6ae108d22c5e29cdb0619f6aee2f195870e
aiminy <jlfmssm@gmail.com>
2017-01-10 10:51:30 -0500

    add bash to use QoRTs

---
+++ inst/bin/UseQoRTs2GetCounts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+infile="$1"
+gtffile="$2"
+qcDataDir="$3"
+
+module load java/1.8.0_60
+
+export _JAVA_OPTIONS="-Xmx36G"
+
+java -jar /nethome/axy148/NGS_tools/QoRTs/QoRTs_1.1.8/QoRTs.jar QC --noGzipOutput "$infile" "$gtffile" "$qcDataDir"
